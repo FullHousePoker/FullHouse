c53e4f090fa6b5760f7bc07141cf43757548a463
euniceschoi <euniceschoi93@gmail.com>
2015-12-23 20:31:48 -0800

    add user and game migration and set up associations

---
+++ db/migrate/20151224041933_create_games.rb
@@ -0,0 +1,14 @@
+class CreateGames < ActiveRecord::Migration
+  def change
+    create_table :games do |t|
+      t.float :lat
+      t.float :lng
+      t.integer :buy_in_amount
+      t.integer :table_size
+      t.text :players, array: true, default: []
+      t.integer :host_id
+      t.string :type
+      t.timestamps null: false
+    end
+  end
+end
